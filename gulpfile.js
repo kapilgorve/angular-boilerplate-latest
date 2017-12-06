70a42bef0d2f197d1611f6e445843368bf1d3863
Kapil Gorve <kapilsg186@gmail.com>
2017-12-06 12:08:13 +0530

    postcss added ,versions updated

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,39 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
 var babel = require('gulp-babel');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+
 gulp.task('sass', function() {
+  return (gulp
+      .src('public/scss/**/*.scss')
 
-    return gulp.src('public/scss/**/*.scss')
-
-        .pipe(sass())
-        //.pipe(concat('css/*.css'))
-        .pipe(gulp.dest('public/css/'));
-
+      .pipe(sass())
+      //.pipe(concat('css/*.css'))
+      .pipe(sourcemaps.init())
+      .pipe(postcss([autoprefixer()]))
+      .pipe(sourcemaps.write('.'))
+      .on('error', function(err) {
+        gutil.log(gutil.colors.red('[Error]'), err.toString());
+      })
+      .pipe(gulp.dest('public/css/')) );
 });
 
-gulp.task('scripts', function () {
-  return gulp.src('public/app/**/*.js')
+gulp.task('scripts', function() {
+  return gulp
+    .src('public/app/**/*.js')
     .pipe(concat('bundle.js'))
     .pipe(gulp.dest('public/'))
     .pipe(babel())
     .pipe(uglify())
-    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+    .on('error', function(err) {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+    })
     .pipe(gulp.dest('public/'));
 });
 
-gulp.task('default', ['sass','scripts'], function() {
-    gulp.watch('public/scss/**/*.scss', ['sass']);
-     gulp.watch('public/app/**/*.js', ['scripts']);
+gulp.task('default', ['sass', 'scripts'], function() {
+  gulp.watch('public/scss/**/*.scss', ['sass']);
+  gulp.watch('public/app/**/*.js', ['scripts']);
 });
-
-
